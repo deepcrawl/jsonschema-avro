279320662ff2a292660ad9a6a195ed59e9f93bb6
Francesco Iesu <francesco.iesu@deepcrawl.com>
2023-11-06 16:52:13 +0000

    chore: fix test promise

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,10 +4,8 @@ import convert from './index'
 describe('index', () => {
   describe('convert()', () => {
     context('without a schema', () => {
-      it('throws', () => {
-        assert.throws(() => {
-          convert()
-        }, new Error('No schema given'))
+      it('throws', async () => {
+        await assert.rejects(() => convert(), new Error('No schema given'))
       })
     })
   })
